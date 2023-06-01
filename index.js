ef4952ca94d8cb94fb0f36d23d4500db7e338ec0
Devsona <122227306+devsona@users.noreply.github.com>
2023-06-01 03:15:05 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+const inputEl = document.getElementById('input-user')
+const imageContainer = document.getElementById('image-container')
+const username = document.getElementById('username')
+const at = document.getElementById('at')
+const joined = document.getElementById('joined')
+
+
+document.getElementById('search-btn').addEventListener('click', function(){
+    fetch(`https://api.github.com/users/${inputEl.value}`, {
+      headers: {
+        'Authorization': 'Bearer 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',
+        'Accept': 'application/vnd.github.v3+json'
+      }
+    })
+      .then(response =>  response.json())
+      .then(data => {
+        console.log(data)
+        imageContainer.innerHTML = `<img class='profile-img' src="${data.avatar_url}" alt='avatar'/>`
+        username.textContent = data.login
+        at.textContent = '@' + data.name
+        joined.textContent = "Joined " + data.created_at.slice(0,4)
+      })
+})
